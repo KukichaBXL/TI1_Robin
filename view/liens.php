b68aa4338d1e1722798247a10118af9146adc0c4
KukichaBXL <craeyerobin@gmail.com>
2026-04-08 13:16:13 +0200

    Create link skeleton

---
+++ view/liens.php
@@ -0,0 +1,32 @@
+<div class="contenu-page">
+
+    <h2 class="titre-page">Liens pour rejoindre les autres capitales</h2>
+
+    <div class="img-centree">
+        <img src="img/liens.jpg" alt="Capitales européennes">
+    </div>
+    <div class="center">
+    <p>Découvrez les sites dédiés aux autres capitales européennes :</p>
+    </div>
+    <ul class="link-liste">
+        <li><a >Berlin</a></li>
+        <li><a >Vienne</a></li>
+        <li><a >Paris</a></li>
+        <li><a >Madrid</a></li>
+        <li><a >Rome</a></li>
+        <li><a >Londres</a></li>
+        <li><a >Lisbonne</a></li>
+        <li><a >Prague</a></li>
+        <li><a >Amsterdam</a></li>
+        <li><a >Copenhague</a></li>
+        <li><a >Dublin</a></li>
+        <li><a >Madrid</a></li>
+        <li><a >Sofia</a></li>
+        <li><a >Varsovie</a></li>
+        <li><a >Helsinki</a></li>
+        <li><a >Stockholm</a></li>
+        <li><a >Oslo</a></li>
+        <li><a >Budapest</a></li>
+    </ul>
+
+</div>
