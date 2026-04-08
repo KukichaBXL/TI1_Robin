ac005023599c71ac95c5f6872eb1717f42297680
KukichaBXL <craeyerobin@gmail.com>
2026-04-08 13:54:35 +0200

    Create config ini

---
+++ config.ini.php
@@ -0,0 +1,13 @@
+<?php 
+
+const ROOT_PATH = __DIR__;
+
+const PUBLIC_PAGES = [
+    'geographie',
+    'histoire',
+    'culture',
+    'galerie',
+    'contact',
+    'liens',
+    'erreur404'
+];
