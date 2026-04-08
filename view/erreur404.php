3bdc02c17813f2c89e3921af0772edd634317314
KukichaBXL <craeyerobin@gmail.com>
2026-04-08 13:15:57 +0200

    Create error404 skeleton

---
+++ view/erreur404.php
@@ -0,0 +1,16 @@
+<div class="contenu-page">
+
+    <h2 class="titre-page">Erreur 404</h2>
+
+    <div class="img-centree">
+        <img src="img/404.jpg" alt="Erreur 404 - Page introuvable">
+    </div>
+
+    <p>
+        Malheureusement nous avons rencontré un problème.
+    </p>
+    <p>
+        <a href="./">← Retourner à l'accueil</a>
+    </p>
+
+</div>
