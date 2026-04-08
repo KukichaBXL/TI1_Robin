035f96d39d4e6f1909605ecefbdd7b318164832f
KukichaBXL <craeyerobin@gmail.com>
2026-04-08 13:16:33 +0200

    Create inc HEADER / MENU

---
+++ view/inc/entete.php
@@ -0,0 +1,5 @@
+<header class="entete">
+    <div class="banniere">
+        <h1 class="titre-capitale">Madrid</h1>
+    </div>
+</header>
---
+++ view/inc/menu.php
@@ -0,0 +1,19 @@
+<nav class="navigation" id="navigation">
+
+    <button class="burger-btn" id="burger-btn" aria-label="Ouvrir le menu">
+        <span class="burger-barre"></span>
+        <span class="burger-barre"></span>
+        <span class="burger-barre"></span>
+    </button>
+
+    <ul class="nav-liste" id="nav-liste">
+        <li><a href="./"              class="nav-lien">Accueil</a></li>
+        <li><a href="./?p=geographie" class="nav-lien">Géographie</a></li>
+        <li><a href="./?p=histoire"   class="nav-lien">Histoire</a></li>
+        <li><a href="./?p=culture"    class="nav-lien">Culture</a></li>
+        <li><a href="./?p=galerie"    class="nav-lien">Galerie</a></li>
+        <li><a href="./?p=contact"    class="nav-lien">Contact</a></li>
+        <li><a href="./?p=liens"      class="nav-lien">Liens</a></li>
+    </ul>
+
+</nav>
