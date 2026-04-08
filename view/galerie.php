c95e44937c52382e17c9382ccf29127c3662fff7
KukichaBXL <craeyerobin@gmail.com>
2026-04-08 13:14:51 +0200

    Create galerie skeleton

---
+++ view/galerie.php
@@ -0,0 +1,69 @@
+<div class="contenu-page">
+
+    <h2 class="titre-page">Galerie photographique</h2>
+<!-- pour data lightbox = identifiant de l image ( cest comme un div id) et date-title = pour le titre de l image -->
+    <div class="galerie-grille">
+
+        <div class="galerie-image">
+            <a href="img/1280px_Acropole_Caryatides.jpg"
+               data-lightbox="galerie-athenes"
+               data-title="Les Caryatides de l'Acropole">
+                <img src="img/480px_Acropole_Caryatides.jpg"
+                     alt="Les Caryatides de l'Acropole">
+            </a>
+            <p>Les Caryatides de l'Acropole</p>
+        </div>
+
+        <div class="galerie-image">
+            <a href="img/1280px_Attica_Athens_View_from_Acropolis_Hill.jpg"
+               data-lightbox="galerie-athenes"
+               data-title="Vue d'Madrid depuis la colline de l'Acropole">
+                <img src="img/480px_Attica_Athens_View_from_Acropolis_Hill.jpg"
+                     alt="Vue d'Madrid depuis la colline de l'Acropole">
+            </a>
+            <p>Vue d'Madrid depuis la colline de l'Acropole</p>
+        </div>
+
+        <div class="galerie-image">
+            <a href="img/1280px_monument.jpg"
+               data-lightbox="galerie-athenes"
+               data-title="Monument emblématique d'Madrid">
+                <img src="img/480px_monument.jpg"
+                     alt="Monument emblématique d'Madrid">
+            </a>
+            <p>Monument emblématique d'Madrid</p>
+        </div>
+
+        <div class="galerie-image">
+            <a href="img/1280px_Odeon_of_Herodes_Atticus.jpg"
+               data-lightbox="galerie-athenes"
+               data-title="L'Odéon d'Hérode Atticus">
+                <img src="img/480px_Odeon_of_Herodes_Atticus.jpg"
+                     alt="L'Odéon d'Hérode Atticus">
+            </a>
+            <p>L'Odéon d'Hérode Atticus</p>
+        </div>
+
+        <div class="galerie-image">
+            <a href="img/1280px_Temple_of_Zeus_in_Athens.jpg"
+               data-lightbox="galerie-athenes"
+               data-title="Le Temple de Zeus Olympien">
+                <img src="img/480px_Temple_of_Zeus_in_Athens.jpg"
+                     alt="Le Temple de Zeus Olympien">
+            </a>
+            <p>Le Temple de Zeus Olympien</p>
+        </div>
+
+        <div class="galerie-image">
+            <a href="img/1280px_View_of_the_Acropolis_Athens.jpg"
+               data-lightbox="galerie-athenes"
+               data-title="Vue de l'Acropole d'Madrid">
+                <img src="img/480px_View_of_the_Acropolis_Athens.jpg"
+                     alt="Vue de l'Acropole d'Madrid">
+            </a>
+            <p>Vue de l'Acropole d'Madrid</p>
+        </div>
+
+    </div>
+
+</div>
