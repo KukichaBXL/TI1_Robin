1f6d9d299da5c3e5f9ac204568086d7f061e9c30
KukichaBXL <craeyerobin@gmail.com>
2026-04-08 13:14:19 +0200

    Create contact skeleton

---
+++ view/contact.php
@@ -0,0 +1,42 @@
+<div class="contenu-page">
+
+    <h2 class="titre-page">Pour nous contacter</h2>
+
+    <div class="img-centree">
+        <img src="img/contacts.jpg" alt="Nous contacter">
+    </div>
+
+    <form action="#" method="post" class="formulaire">
+
+        <div class="form-ligne">
+            <label for="nom">Nom :</label>
+            <input type="text" id="nom" name="nom" placeholder="Votre nom">
+        </div>
+
+        <div class="form-ligne">
+            <label for="prenom">Prénom :</label>
+            <input type="text" id="prenom" name="prenom" placeholder="Votre prénom">
+        </div>
+
+        <div class="form-ligne">
+            <label for="email">E-mail :</label>
+            <input type="email" id="email" name="email" placeholder="votre@email.com">
+        </div>
+
+        <div class="form-ligne">
+            <label for="sujet">Sujet :</label>
+            <input type="text" id="sujet" name="sujet" placeholder="Sujet de votre message">
+        </div>
+
+        <div class="form-ligne">
+            <label for="message">Message :</label>
+            <textarea id="message" name="message" placeholder="Votre message..."></textarea>
+        </div>
+
+        <div class="form-bouton">
+            <button type="submit">Envoyer le message</button>
+        </div>
+
+    </form>
+
+</div>
