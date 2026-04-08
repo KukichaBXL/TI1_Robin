b65d382518b0f064a7d5fae9440d3cc453a160a9
KukichaBXL <craeyerobin@gmail.com>
2026-04-08 15:36:12 +0200

    Lightbox petit bonus

--- view/galerie.php
+++ view/galerie.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Madrid - Galerie</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="css/lightbox.css">
 </head>
 <body>
 
@@ -24,13 +25,19 @@
     <div class="galerie-grille">
 
         <div class="galerie-image">
-            <img src="img/480px_Buen_Retiro_Park.jpg"
+            <a href="img/1280px_Buen_Retiro_Park.jpg"
+               data-lightbox="galerie-madrid"
+               data-title="Les Caryatides de l'Acropole">
+                <img src="img/480px_Buen_Retiro_Park.jpg"
                      alt="Buen Retiro Park">
             </a>
             <p>Buen Retiro Park</p>
         </div>
 
         <div class="galerie-image">
+            <a href="img/1280px_Calle_de_Alcala.jpg"
+               data-lightbox="galerie-madrid"
+               data-title="Calle de Alcala">
                 <img src="img/480px_Calle_de_Alcala.jpg"
                      alt="Calle de Alcala">
             </a>
@@ -38,6 +45,9 @@
         </div>
 
         <div class="galerie-image">
+            <a href="img/1280px_Congreso_de_los_Diputados.jpg"
+               data-lightbox="galerie-madrid"
+               data-title="Congreso de los Diputados">
                 <img src="img/480px_Congreso_de_los_Diputados.jpg"
                      alt="Congreso de los Diputados">
             </a>
@@ -45,6 +55,9 @@
         </div>
 
         <div class="galerie-image">
+            <a href="img/1280px_Palacio_de_Comunicaciones.jpg"
+               data-lightbox="galerie-madrid"
+               data-title="Palacio de Comunicaciones">
                 <img src="img/480px_Palacio_de_Comunicaciones.jpg"
                      alt="Palacio de Comunicaciones">
             </a>
@@ -52,6 +65,9 @@
         </div>
 
         <div class="galerie-image">
+            <a href="img/1280px_Palacio_Real.jpg"
+               data-lightbox="galerie-madrid"
+               data-title="Palacio_Real">
                 <img src="img/480px_Palacio_Real.jpg"
                      alt="Palacio Real">
             </a>
@@ -59,6 +75,9 @@
         </div>
 
         <div class="galerie-image">
+            <a href="img/1280px_Plaza_de_Colón.jpg"
+               data-lightbox="galerie-madrid"
+               data-title="Plaza_de_Colón">
                 <img src="img/480px_Plaza_de_Colón.jpg"
                      alt="Plaza_de_Colón">
             </a>
@@ -72,6 +91,7 @@
 
     </div>
 
+<script src="js/lightbox-plus-jquery.js"></script>
 <script src="js/menu.js"></script>
 </body>
 </html>
